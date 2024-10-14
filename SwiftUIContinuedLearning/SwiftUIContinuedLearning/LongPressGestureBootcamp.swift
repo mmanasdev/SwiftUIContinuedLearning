d27e7f3adcc00c9e891e7e6bf40e7dd8256ed49c
miguelmanasdev <miguel.manas.alvarez.dev@gmail.com>
2024-10-14 21:55:43 +0200

    LongPressGestureBootcamp

---
+++ SwiftUIContinuedLearning/SwiftUIContinuedLearning/LongPressGestureBootcamp.swift
@@ -0,0 +1,85 @@
+//
+//  LongPressGestureBootcamp.swift
+//  SwiftUIContinuedLearning
+//
+//  Created by Miguel Mañas Alvarez on 14/10/24.
+//
+
+import SwiftUI
+
+struct LongPressGestureBootcamp: View {
+    
+    @State var isComplete: Bool = false
+    @State var isSuccess: Bool = false
+    
+    var body: some View {
+        /*
+        Text(isComplete ? "COMPLETE" : "NOT COMPLETE")
+            .padding()
+            .padding(.horizontal)
+            .background(isComplete ? Color.green : Color.gray)
+            .cornerRadius(10)
+//            .onTapGesture {
+//                isComplete.toggle()
+//            }
+            .onLongPressGesture(minimumDuration: 3.0, maximumDistance: 1.0) {
+                isComplete.toggle()
+            }
+         */
+        
+        VStack {
+            Rectangle()
+                .fill(isSuccess ? Color.green : Color.blue)
+                .frame(maxWidth: isComplete ? .infinity : 0)
+                .frame(height: 55)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .background(Color.gray)
+            
+            HStack {
+                Text("CLICK HERE")
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.black)
+                    .cornerRadius(10)
+                    .onLongPressGesture(minimumDuration: 1.0, maximumDistance: 50) { isPressing in
+                        // starrt of press -> min duration
+                        if isPressing {
+                            withAnimation(.easeInOut(duration: 1.0)) {
+                                isComplete = true
+                            }
+                        } else {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                                if !isSuccess {
+                                    withAnimation(.easeInOut) {
+                                        isComplete = false
+                                    }
+                                }
+                            }
+                        }
+                    } perform: {
+                        // at the min duration
+                        withAnimation(.easeInOut) {
+                            isSuccess.toggle()
+                        }
+                    }
+
+                
+                Text("RESET")
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.black)
+                    .cornerRadius(10)
+                    .onTapGesture {
+                        isComplete = false
+                        isSuccess = false
+                    }
+            }
+        }
+    }
+}
+
+struct LongPressGestureBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        LongPressGestureBootcamp()
+    }
+}
