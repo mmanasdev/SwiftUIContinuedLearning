eb6168f9cce62d85a3584e04d266d4185c55dcd5
miguelmanasdev <miguel.manas.alvarez.dev@gmail.com>
2024-10-14 21:35:19 +0200

    start xcode project

---
+++ SwiftUIContinuedLearning/SwiftUIContinuedLearning/SwiftUIContinuedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIContinuedLearningApp.swift
+//  SwiftUIContinuedLearning
+//
+//  Created by Miguel Mañas Alvarez on 14/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIContinuedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
