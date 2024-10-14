f211e6dd411d40bbdd876eb49323ba857cbf5304
miguelmanasdev <miguel.manas.alvarez.dev@gmail.com>
2024-10-14 23:19:10 +0200

    RotationGestureBootcamp

---
+++ SwiftUIContinuedLearning/SwiftUIContinuedLearning/RotationGestureBootcamp.swift
@@ -0,0 +1,41 @@
+//
+//  RotationGestureBootcamp.swift
+//  SwiftUIContinuedLearning
+//
+//  Created by Miguel Mañas Alvarez on 14/10/24.
+//
+
+import SwiftUI
+
+struct RotationGestureBootcamp: View {
+    
+    @State var angle: Angle = Angle(degrees: 0)
+    
+    var body: some View {
+        Text("Hello, World!")
+            .font(.largeTitle)
+            .fontWeight(.semibold)
+            .foregroundColor(.white)
+            .padding(50)
+            .background(Color.blue.cornerRadius(10))
+            .rotationEffect(angle)
+            .gesture(
+                RotationGesture()
+                    .onChanged { value in
+                        angle = value
+                    }
+                    .onEnded { value in
+                        withAnimation(.spring()) {
+                            angle = Angle(degrees: 0)
+                        }
+                    }
+            )
+        
+    }
+}
+
+struct RotationGestureBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        RotationGestureBootcamp()
+    }
+}
