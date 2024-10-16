04436e459950370c1ac3d07c6729828b07eafb95
miguelmanasdev <miguel.manas.alvarez.dev@gmail.com>
2024-10-16 19:53:33 +0200

    ScrollViewReader to auto scroll in SwiftUI | Continued Learning #5

---
+++ SwiftUIContinuedLearning/SwiftUIContinuedLearning/ScrollViewReaderBootcamp.swift
@@ -0,0 +1,60 @@
+//
+//  ScrollViewReaderBootcamp.swift
+//  SwiftUIContinuedLearning
+//
+//  Created by Miguel Mañas Alvarez on 16/10/24.
+//
+
+import SwiftUI
+
+struct ScrollViewReaderBootcamp: View {
+    
+    @State var scrollToIndex: Int = 0
+    @State var textFieldText: String = ""
+    
+    var body: some View {
+        VStack {
+            TextField("Enter a # here...", text: $textFieldText)
+                .frame(height: 55)
+                .border(Color.gray)
+                .padding(.horizontal)
+                .keyboardType(.numberPad)
+            
+            Button("Scroll now") {
+                withAnimation(.spring()) {
+                    if let index = Int(textFieldText) {
+                        scrollToIndex = index
+                    }
+                }
+            }
+            
+            ScrollView {
+                ScrollViewReader { proxy in
+                    
+                    ForEach(0..<50) { index in
+                        Text("This is item number #\(index)")
+                            .font(.headline)
+                            .frame(height: 200)
+                            .frame(maxWidth: .infinity)
+                            .background(Color.white)
+                            .cornerRadius(10)
+                            .shadow(radius: 10)
+                            .padding()
+                            .id(index)
+                    }
+                    .onChange(of: scrollToIndex) { value in
+                        withAnimation(.spring()) {
+                            proxy.scrollTo(value, anchor: .top)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ScrollViewReaderBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrollViewReaderBootcamp()
+    }
+}
