510f6d123c1297dd20b811ef99af1e1a8eb1b267
miguelmanasdev <miguel.manas.alvarez.dev@gmail.com>
2024-11-12 18:21:46 +0100

    Save data and images to FileManager in Xcode | Continued Learning #26 p1

---
+++ SwiftUIContinuedLearning/SwiftUIContinuedLearning/FileManagerBootcamp.swift
@@ -0,0 +1,53 @@
+//
+//  FileManagerBootcamp.swift
+//  SwiftUIContinuedLearning
+//
+//  Created by Miguel Mañas Alvarez on 12/11/24.
+//
+
+import SwiftUI
+
+class FileManagerViewModel: ObservableObject {
+    
+    @Published var image: UIImage? = nil
+    let imageName: String = "forest-background"
+    
+    init() {
+        getImageFromAssetsFolder()
+    }
+    
+    func getImageFromAssetsFolder() {
+        image = UIImage(named: imageName)
+    }
+    
+}
+
+struct FileManagerBootcamp: View {
+    
+    @StateObject var vm = FileManagerViewModel()
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                
+                if let image = vm.image {
+                    Image(uiImage: image)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 200, height: 200)
+                        .clipped()
+                        .cornerRadius(10)
+                }
+                
+                Spacer()
+            }
+            .navigationTitle("File Manager")
+        }
+    }
+}
+
+struct FileManagerBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        FileManagerBootcamp()
+    }
+}
